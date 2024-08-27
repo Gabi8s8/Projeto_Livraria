526951165b1bdbdd4fde0fe554334789ac6976b0
Gabi8s8 <gabriela.teixeira1@aluno.ifsp.edu.br>
2024-08-27 08:38:45 -0300

    Terminando Header e Iniciando Banner

---
+++ src/componentes/SecaoCapa/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+const SecaoCapa = () => {
+    return (
+        <section className='secao-capa'>
+            <div className='capa center'>
+                <div className='texto-capa'>
+                    <p>Entre</p>
+                    <h1>Páginas</h1>
+                    <p>Encontre</p>
+                    <h1>Mundos</h1>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SecaoCapa
