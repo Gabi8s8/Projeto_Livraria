b5c50d9944cf3da26883fdb266d096bceb167c0c
Gabi8s8 <gabriela.teixeira1@aluno.ifsp.edu.br>
2024-09-17 08:45:17 -0300

    Iniciando Produtos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './componentes/Header'
 import SecaoCapa from './componentes/SecaoCapa';
+import Produtos from './componentes/Produtos'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <SecaoCapa/>
+      <Produtos/>
     </div>
   );
 }
---
+++ src/componentes/Produtos/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './style.css';
+
+const Produtos = () => {
+    return (
+        <section id="produtos">
+            <h2 className="titulos">PRODUTOS</h2>
+            <p>Trabalhamos com  livros de diversos gêneros: romance, suspense, distopia, fantasia e muitos outros!</p>
+
+            <p>Todos os nossos preços são acessíveis e contam com a melhor qualidade do mercado</p>
+            
+        </section>
+    )
+}
+
+export default Produtos;
